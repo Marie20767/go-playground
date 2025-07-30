668050da6522dbe1c7bc6eba789a84ffd5cc395c
Marie20767 <marie20767@gmail.com>
2025-07-30 14:27:02 +0200

    Added AreBracketsValid with tests

---
+++ algorithms/arrays/stacks/stacks.go
@@ -0,0 +1,12 @@
+package stacks
+
+// TODO: create a stack that you can just pop and push to 
+// pop means that you can either remove the last element or get the last element, vice versa for push
+
+// func IsStackValid(s string) bool {
+// 	closeToOpenBracketsMap := map[string]string{
+// 		")" : "(", 
+// 		"]" : "[", 
+// 		"}" : "{",
+// 	}
+// }
