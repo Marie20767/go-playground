a42495c245a64094d0325a0179ba5bd611021bc7
Marie20767 <marie20767@gmail.com>
2025-08-29 11:06:55 +0200

    Improved shift

--- algorithms/strings/shift.go
+++ algorithms/strings/shift.go
@@ -1,21 +1,15 @@
 package shift
 
-import (
-	"strings"
-)
-
-
 func Shift(s string) string {
-	alphabet := "abcdefghijklmnopqrstuvwxyz"
 	shifted := []rune(s)
 
 	for i, char := range s {
-		currentCharI := strings.Index(alphabet, string(char))
-
-		if currentCharI >= len(alphabet)-1 {
-			shifted[i] = rune(alphabet[0])
-		} else {
-			shifted[i] = rune(alphabet[currentCharI+1])
+		if char >= 'a' && char <= 'z' {
+			if char == 'z' {
+				shifted[i] = 'a'
+			} else {
+				shifted[i] = char + 1
+			}
 		}
 	}
 
