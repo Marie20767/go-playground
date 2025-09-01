21d57dd8ca3b23e12f786f980d3b089acadbf83a
Marie20767 <marie20767@gmail.com>
2025-09-01 18:57:31 +0200

    Renamed merge func

--- algorithms/sorting/merge/merge_sort.go
+++ algorithms/sorting/merge/merge_sort.go
@@ -1,41 +1,6 @@
 package merge_sort
 
-func SortMerged(nums1 []int, nums2 []int) []int {
-	if len(nums1) == 0 {
-		return append([]int{}, nums2...)
-	}
-
-	if len(nums2) == 0 {
-		return append([]int{}, nums1...)
-	}
-
-	merged := make([]int, 0, len(nums1)+len(nums2))
-	i, j := 0, 0
-
-	for i < len(nums1) && j < len(nums2) {
-		if nums1[i] < nums2[j] {
-			merged = append(merged, nums1[i])
-			i++
-		} else {
-			merged = append(merged, nums2[j])
-			j++
-		}
-	}
-
-	for i < len(nums1) {
-		merged = append(merged, nums1[i])
-		i++
-	}
-
-	for j < len(nums2) {
-		merged = append(merged, nums2[j])
-		j++
-	}
-
-	return merged
-}
-
-func merge(left, right []int) []int {
+func mergeSortedLists(left, right []int) []int {
 	result := []int{}
 	i, j := 0, 0
 
@@ -64,5 +29,5 @@ func MergeSort(nums []int) []int {
 	left := MergeSort(nums[:mid])
 	right := MergeSort(nums[mid:])
 
-	return merge(left, right)
+	return mergeSortedLists(left, right)
 }
--- algorithms/sorting/merge/merge_sort_test.go
+++ algorithms/sorting/merge/merge_sort_test.go
@@ -7,18 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSortMerged(t *testing.T) {
-	t.Run("should merge and sort 2 slices", func(t *testing.T) {
-		nums1 := []int{1, 20, 100, 500}
-		nums2 := []int{2, 3, 4, 5, 6}
-
-		expected := []int{1, 2, 3, 4, 5, 6, 20, 100, 500}
-		actual := merge_sort.SortMerged(nums1, nums2)
-
-		assert.Equal(t, expected, actual)
-	})
-}
-
 func TestMergeSort(t *testing.T) {
 	t.Run("should sort slice with merge sort", func(t *testing.T) {
 		nums := []int{100, 5, 1, 1000, 10}
