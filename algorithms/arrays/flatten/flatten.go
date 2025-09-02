76999e09f7d6911d8158609756ec0889fb58b48c
Marie20767 <marie20767@gmail.com>
2025-09-02 13:22:28 +0200

    Added generics to flatten

--- algorithms/arrays/flatten/flatten.go
+++ algorithms/arrays/flatten/flatten.go
@@ -1,14 +1,14 @@
 package flatten
 
-func Flatten(elements []any) []any{
-	res := make([]any, 0, len(elements))
+func Flatten[T any](elements []any) []T{
+	var res []T
 
 	for _, el := range elements {
 		if nested, isNested := el.([]any); isNested {
-			flattened := Flatten(nested)
+			flattened := Flatten[T](nested)
 			res = append(res, flattened...)
 		} else {
-			res = append(res, el)
+			res = append(res, el.(T))
 		}
 	}
 
