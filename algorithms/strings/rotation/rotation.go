8596462dba3f7c5350bf936ec973ca1fac92ee02
Marie20767 <marie20767@gmail.com>
2025-09-02 15:27:14 +0200

    Added notes in rotation

--- algorithms/strings/rotation/rotation.go
+++ algorithms/strings/rotation/rotation.go
@@ -10,6 +10,7 @@ func rotate(s string) string {
 	return s[1:] + s[:1]
 }
 
+// time complexity: O(n²) as reassigning string is O(n)
 func IsRotation(s1 string, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
@@ -28,6 +29,7 @@ func IsRotation(s1 string, s2 string) bool {
 	return false
 }
 
+// time complexity O(n)
 func IsRotation2(s1 string, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
